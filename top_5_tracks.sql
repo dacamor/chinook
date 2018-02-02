ea0ca93593fb8537ae5e6dd292e3d37b6da5cec5
Sean Williams <dacamor85@gmail.com>
2018-02-02 09:49:19 -0600

    updated top_5_tracks.sql

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,8 +1,6 @@
-Select song.*
-from (select Track.Name as Name, count(Invoice.Total) as count
-			from InvoiceLine
-			join Invoice On InvoiceLine.InvoiceId = Invoice.InvoiceId
-			join Track On InvoiceLine.TrackId = Track.TrackId
-			Group by InvoiceLine.TrackId) song
-Order by song.count DESC
+select Track.Name as Name, count(InvoiceLine.Quantity) as count
+from InvoiceLine
+join Track On InvoiceLine.TrackId = Track.TrackId
+Group by InvoiceLine.TrackId
+order by count desc
 Limit 5
